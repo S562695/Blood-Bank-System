1edbf8bbdae9c16c002e408803e4e5298f22ac17
Naga Lakshmi Konanki <123758623+S562695@users.noreply.github.com>
2024-03-29 16:03:29 -0500

    Delete Blood Bank System/Blood-Bank-System directory

--- Blood Bank System/Blood-Bank-System/ViewController/Chat/Model/MessageType.swift
+++
@@ -1,8 +0,0 @@
-enum MessageType: String, Codable {
-    case TEXT = "TEXT"
-    case IMAGE = "IMAGE"
-    case VIDEO = "VIDEO"
-    case LOCATION = "LOCATION"
-    case CONTACT = "CONTACT"
-    case FILE = "FILE"
-}
